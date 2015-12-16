2d7787702a6ab75a826b447d4db7359ec110ffef
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-16 14:51:14 +0100

    run perceptual_cqt in parallel with joblib

--- DeepInstruments/__init__.py
+++ DeepInstruments/__init__.py
@@ -5,37 +5,43 @@ import numpy as np
 import os
 import sys
 
+fmin = 55  # in Hertz
+n_octaves = 7
+n_bins_per_octave = 24
+sr = 32000.0  # in Hertz
+hop_duration = 0.016  # in seconds
+decision_duration = 2.048  # in seconds
 dataset_dir = '/Users/vlostan/datasets/solosDb8'
-wav_paths = get_paths(dataset_dir, 'wav')
+file_paths = get_paths(dataset_dir, 'wav')
 
+# Run perceptual CQT in parallel with joblib
+# n_jobs = -1 means that are CPUs are used
+Parallel(n_jobs=-1)(perceptual_cqt(file_path,
+                                   decision_duration,
+                                   fmin,
+                                   hop_duration,
+                                   n_bins_per_octave,
+                                   n_octaves,
+                                   sr) for file_path in file_paths)
 
 def get_paths(dir, extension):
     walk = os.walk(dir)
     regex = '*.' + extension
     return [p for d in walk for p in glob.glob(os.path.join(d[0], regex))]
 
-
-fmin = 55  # in Hertz
-n_octaves = 7
-n_bins_per_octave = 24
-sr = 32000.0  # in Hertz
-hop_time = 0.016  # in seconds
-decision_time = 2.048  # in seconds
-
-
 def perceptual_cqt(
         file_path,
         decision_duration,
         fmin,
         hop_duration,
-        n_bins,
         n_bins_per_octave,
+        n_octaves,
         sr):
     y, y_sr = librosa.load(file_path)
     if y_sr != sr:
         y = librosa.resample(y, y_sr, sr)
-    hop_length = hop_time * sr
-    decision_length = decision_time * sr / hop_length
+    hop_length = hop_duration * sr
+    decision_length = decision_duration * sr / hop_length
     n_bins = n_octaves * n_bins_per_octave
     freqs = librosa.cqt_frequencies(
             bins_per_octave=n_bins_per_octave,
