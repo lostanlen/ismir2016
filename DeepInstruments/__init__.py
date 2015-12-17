33168eaadb9fc0cdc078553a579bad0a4f9d028c
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-17 11:43:27 +0100

    write get_instrument function

--- DeepInstruments/__init__.py
+++ DeepInstruments/__init__.py
@@ -14,11 +14,13 @@ n_bins_per_octave = 24
 sr = 32000.0  # in Hertz
 hop_duration = 0.016  # in seconds
 decision_duration = 2.048  # in seconds
+instrument_list = ['Cl', 'Co', 'Fh', 'Gt', 'Ob', 'Pn', 'Tr', 'Vl']
 file_paths = get_paths(dataset_dir, 'wav')
+
+file_instruments = map(get_instrument, file_paths)
 
 # Run perceptual CQT in parallel with joblib
 # n_jobs = -1 means that all CPUs are used
-
 file_cqts = Parallel(n_jobs=-1, verbose=10)(delayed(cached_cqt)(
         file_path,
         decision_duration,
@@ -37,6 +39,10 @@ def get_paths(dir, extension):
     walk = os.walk(dir)
     regex = '*.' + extension
     return [p for d in walk for p in glob.glob(os.path.join(d[0], regex))]
+
+def get_instrument(file_path, instrument_list):
+    instrument_str = os.path.split(os.path.split(file_path)[0])[1]
+    return instrument_list.index(instrument_str)
 
 def perceptual_cqt(
         file_path,
