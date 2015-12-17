4a50c21df4e23849a2d0934018ab3600ebfa4842
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-17 16:13:47 +0100

    import keras

--- DeepInstruments/__init__.py
+++ DeepInstruments/__init__.py
@@ -1,12 +1,14 @@
 from joblib import Memory, Parallel, delayed
 import glob
+import keras
 import librosa
 import numpy as np
+import numpy.matlib
 import os
 
 dataset_dir = '~/datasets/solosDb8/train'
 memory = Memory(cachedir='solosDb8_train')
-cached_cqt = memory.cache(perceptual_cqt)
+cached_cqt = memory.cache(perceptual_cqt, verbose=0)
 
 fmin = 55  # in Hertz
 n_octaves = 7
@@ -17,11 +19,9 @@ decision_duration = 2.048  # in seconds
 instrument_list = ['Cl', 'Co', 'Fh', 'Gt', 'Ob', 'Pn', 'Tr', 'Vl']
 file_paths = get_paths(dataset_dir, 'wav')
 
-file_instruments = map(get_instrument, file_paths)
-
 # Run perceptual CQT in parallel with joblib
 # n_jobs = -1 means that all CPUs are used
-file_cqts = Parallel(n_jobs=-1, verbose=10)(delayed(cached_cqt)(
+file_cqts = Parallel(n_jobs=-1, verbose=20)(delayed(cached_cqt)(
         file_path,
         decision_duration,
         fmin,
@@ -31,7 +31,35 @@ file_cqts = Parallel(n_jobs=-1, verbose=10)(delayed(cached_cqt)(
         sr) for file_path in file_paths)
 
 # Reduce all CQTs into one
-cqts = np.vstack(file_cqts)
+item_cqts = np.vstack(file_cqts)
+
+file_instruments = [get_instrument(p, instrument_list) for p in file_paths]
+n_items_per_file = [cqt.shape[0] for cqt in file_cqts]
+item_instruments = expand_instruments(file_instruments, n_items_per_file)
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+
+model = Sequential()
+
+conv1 = Convolution2D(32, 3, 3, input_shape=(1,100,100))
+model.add(conv1)
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
 
 def get_paths(dir, extension):
@@ -40,9 +68,18 @@ def get_paths(dir, extension):
     regex = '*.' + extension
     return [p for d in walk for p in glob.glob(os.path.join(d[0], regex))]
 
+
 def get_instrument(file_path, instrument_list):
     instrument_str = os.path.split(os.path.split(file_path)[0])[1]
-    return instrument_list.index(instrument_str)
+    n_instruments = len(instrument_list)
+    instrument_onehot = np.zeros(n_instruments)
+    instrument_onehot[instrument_list.index(instrument_str)] = 1.0
+    return instrument_onehot
+
+def expand_instruments(fs, ns):
+    items = [ numpy.matlib.repmat(fs[i], ns[i], 1) for i in range(len(fs))]
+    return np.vstack(items)
+
 
 def perceptual_cqt(
         file_path,
