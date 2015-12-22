a0eb7fc93260804c3ac9e0cc672cdf76651ce46f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-22 10:59:20 +0100

    define file symbolic.py

---
+++ DeepInstruments/symbolic.py
@@ -0,0 +1,12 @@
+
+def expand_instruments(fs, ns):
+    items = [ numpy.matlib.repmat(fs[i], ns[i], 1) for i in range(len(fs))]
+    return np.vstack(items)
+
+
+def get_instrument(file_path, instrument_list):
+    instrument_str = os.path.split(os.path.split(file_path)[0])[1]
+    n_instruments = len(instrument_list)
+    instrument_onehot = np.zeros(n_instruments)
+    instrument_onehot[instrument_list.index(instrument_str)] = 1.0
+    return instrument_onehot
