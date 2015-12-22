869e52bf328a4c5cea099d2ecb9d77310702b152
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-22 11:34:48 +0100

    move script to sandbox

--- DeepInstruments/__init__.py
+++ DeepInstruments/__init__.py
@@ -6,36 +6,7 @@ import numpy as np
 import numpy.matlib
 import os
 
-import audio
-import rwc
-import solosdb
-import symbolic
-
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
-
-
-fmin = librosa.note_to_hz('A1')  # in Hertz
-n_octaves = 7
-n_bins_per_octave = 24
-sr = 32000.0  # in Hertz
-hop_duration = 0.016  # in seconds
-decision_duration = 2.048  # in seconds
-instrument_list = ['Cl', 'Co', 'Fh', 'Gt', 'Ob', 'Pn', 'Tr', 'Vl']
-n_instruments = len(instrument_list)
-
-solosDb8train_dir = '~/datasets/solosDb8/train'
-(X_train, Y_train) = solosdb.get_XY(
-        symbolic.get_paths(solosDb8train_dir, instrument_list, 'wav'),
-        instrument_list,
-        decision_duration, fmin, hop_duration, n_bins_per_octave, n_octaves, sr)
-input_shape = X_train.shape[1:]
-
-rwc8_dir = '~/datasets/rwc8/'
-rwc_paths = symbolic.get_paths(rwc8_dir, instrument_list, 'wav')
-pooling_strides = np.array([2, 2])
-
-file_paths = get_paths('~/datasets/rwc8', instrument_list, 'wav')
-midis = [get_RWC_midi(p, rwc_offsets) for p in file_paths]
---
+++ DeepInstruments/sandbox.py
@@ -0,0 +1,20 @@
+
+fmin = librosa.note_to_hz('A1')  # in Hertz
+n_octaves = 7
+n_bins_per_octave = 24
+sr = 32000.0  # in Hertz
+hop_duration = 0.016  # in seconds
+decision_duration = 2.048  # in seconds
+instrument_list = ['Cl', 'Co', 'Fh', 'Gt', 'Ob', 'Pn', 'Tr', 'Vl']
+n_instruments = len(instrument_list)
+
+solosDb8train_dir = '~/datasets/solosDb8/train'
+(X_train, Y_train) = solosdb.get_XY(
+        symbolic.get_paths(solosDb8train_dir, instrument_list, 'wav'),
+        instrument_list,
+        decision_duration, fmin, hop_duration, n_bins_per_octave, n_octaves, sr)
+input_shape = X_train.shape[1:]
+
+rwc8_dir = '~/datasets/rwc8/'
+rwc_paths = symbolic.get_paths(rwc8_dir, instrument_list, 'wav')
+midis = [ rwc.get_midi(p, rwc.midi_offsets) for p in rwc_paths ]
