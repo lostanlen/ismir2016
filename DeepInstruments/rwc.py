37b46ea2ed22638cad57c318d9ba992bd19ff2a2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-22 11:06:13 +0100

    update rwc.py

--- DeepInstruments/rwc.py
+++ DeepInstruments/rwc.py
@@ -1,3 +1,10 @@
+from joblib import Memory, Parallel, delayed
+import numpy as np
+import os
+
+import audio
+import symbolic
+
 def get_midi(file_path, offset_dictionary):
     instrument_str = os.path.split(os.path.split(file_path)[0])[1]
     file_index = os.path.split(os.path.split(file_path)[1])[1].split('_')[1].split('.')[0]
@@ -15,6 +22,8 @@ def get_XY(
         sr):
     # Run perceptual CQT in parallel with joblib
     # n_jobs = -1 means that all CPUs are used
+    memory = Memory(cachedir='/tmp/joblib')
+    cached_cqt = memory.cache(audio.perceptual_cqt, verbose=0)
     file_cqts = Parallel(n_jobs=-1, verbose=20)(delayed(cached_cqt)(
         file_path,
         decision_duration,
@@ -24,11 +33,11 @@ def get_XY(
         n_octaves,
         sr) for file_path in file_paths)
     # Get first window of each file and stack according to first dimension
-    X = np.vstack([file_cqt[0, :, :] for file_cqts in file_cqts])
+    X = np.vstack([file_cqt[0, :, :] for file_cqt in file_cqts])
     # Reshape to Theano-friendly format
     new_shape = X.shape
     new_shape = (new_shape[0], 1, new_shape[1], new_shape[2])
     X = np.reshape(X, new_shape)
-    file_instruments = [get_instrument(p, instrument_list) for p in file_paths]
+    file_instruments = [symbolic.get_instrument(p, instrument_list) for p in file_paths]
     Y = np.vstack(file_instruments)
     return (X, Y)
