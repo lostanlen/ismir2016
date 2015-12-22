79bce44f311004b4bf41e97620febaadb887f14f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-22 11:45:38 +0100

    import submodules in __init__

--- DeepInstruments/__init__.py
+++ DeepInstruments/__init__.py
@@ -10,3 +10,8 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
+
+import DeepInstruments.audio
+import DeepInstruments.rwc
+import DeepInstruments.solosdb
+import DeepInstruments.symbolic
