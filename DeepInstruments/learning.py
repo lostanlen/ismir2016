b2894e066efa6e7c13fc441c3127d6148ae5f7b3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-23 09:41:04 +0100

    import LeakyReLU

--- DeepInstruments/learning.py
+++ DeepInstruments/learning.py
@@ -1,5 +1,6 @@
 from keras.models import Graph
 
+from keras.layers.advances_activations import LeakyReLU
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
@@ -17,8 +18,9 @@ def build_graph(
         conv2_width,
         pool2_height,
         pool2_width,
-        drop_proportion,
+        drop1_proportion,
         dense1_channels,
+        drop2_proportion,
         dense2_channels):
     graph = Graph()
 
@@ -26,3 +28,34 @@ def build_graph(
 
     conv1 = Convolution2D(conv1_channels, conv1_height, conv1_width)
     graph.add_node(conv1, name="conv1", input="X")
+
+    # relu1
+
+    pool1 = MaxPooling2D(pool_size=(pool1_height, pool1_width))
+    graph.add_node(pool1, name="pool1", input="conv1")
+
+    conv2 = Convolution2D(conv2_channels, conv2_height, conv2_width)
+    graph.add_node(conv2, name="conv2", input="pool1")
+
+    # relu2
+
+
+    pool2 = MaxPooling2D(pool_size=(pool2_height, pool2_width))
+    graph.add_node(pool2, name="pool2", input="conv2")
+
+    flatten = Flatten()
+    graph.add_node(flatten, name="flatten", input="pool2")
+
+    drop1 = Dropout(drop1_proportion)
+    graph.add_node(flatten, name="drop1", input="flatten")
+
+    dense1 = Dense(dense1_channels, activation="relu")
+    graph.add_node(dense1, name="dense1", input="drop1")
+
+    drop2 = Dropout(drop2_proportion)
+    graph.add_node(drop2, name="drop2", input="dense1")
+
+    dense2 = Dense(dense2_channels, activation="drop2")
+    graph.add_node(name="dense2", input="drop2")
+
+    graph.add_output(name="Y", input="dense2")
