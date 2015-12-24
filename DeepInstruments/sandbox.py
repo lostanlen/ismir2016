1ba21097de4114a797cf5f3e674e670e53c501a3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-24 15:01:42 +0100

    batch_size=1 yields a 1-layer ConvNet that actually works

--- DeepInstruments/sandbox.py
+++ DeepInstruments/sandbox.py
@@ -23,28 +23,50 @@ file_path = file_paths[0]
         instrument_list, decision_duration, fmin, hop_duration,
         n_bins_per_octave, n_octaves, silence_threshold, sr)
 
-X_train = np.reshape(X_train, (X_train.shape[0], np.prod(X_train.shape[1:])))
+from keras.models import Graph
 
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import Adagrad
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
-model = Sequential()
-# Dense(64) is a fully-connected layer with 64 hidden units.
-# in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
-model.add(Dense(256, input_dim=21504, init='uniform'))
-model.add(Activation('tanh'))
-model.add(Dropout(0.5))
-model.add(Dense(128, init='uniform'))
-model.add(Activation('tanh'))
-model.add(Dropout(0.5))
-model.add(Dense(8, init='uniform'))
-model.add(Activation('softmax'))
+
+X_height = 168
+X_width = 128
+conv1_channels = 2
+conv1_height = 96
+conv1_width = 32
+pool1_height = 8
+pool1_width = 8
+conv2_channels = 3
+conv2_height = 8
+conv2_width = 8
+pool2_height = 3
+pool2_width = 3
+drop1_proportion = 0.5
+dense1_channels = 25
+drop2_proportion = 0.5
+dense2_channels = 8
+
+
+graph = Graph()
+
+graph.add_input(name="X", input_shape=(1, X_height, X_width))
+
+conv1 = Convolution2D(conv1_channels, conv1_height, conv1_width)
+graph.add_node(conv1, name="conv1", input="X")
+
+relu1 = LeakyReLU()
+graph.add_node(relu1, name="relu1", input="conv1")
+
+flatten = Flatten()
+graph.add_node(flatten, name="flatten", input="relu1")
+
+dense2 = Dense(dense2_channels, activation="relu")
+graph.add_node(dense2, name="dense2", input="flatten")
+
+graph.add_output(name="Y", input="dense2")
+
+
 adagrad = keras.optimizers.Adagrad(lr=0.01, epsilon=1e-06)
-model.compile(loss='mean_squared_error', optimizer=adagrad)
-model.fit(X_train, Y_train, nb_epoch=10, batch_size=16)
-
-
 graph.compile(loss={'Y':'mean_squared_error'}, optimizer=adagrad)
-history = graph.fit({'X':X_train, 'Y':Y_train}, nb_epoch=1)
+history = graph.fit({'X':X_train, 'Y':Y_train}, nb_epoch=1, batch_size=1)
