6e03fa436932a5b662c01fdd4f72b92297ace694
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-27 12:10:15 +0100

    reorganize sandbox

--- DeepInstruments/sandbox.py
+++ DeepInstruments/sandbox.py
@@ -5,6 +5,7 @@ import os
 
 import DeepInstruments as di
 
+# Audio settings
 fmin = librosa.note_to_hz('A1')  # in Hertz
 n_octaves = 7
 n_bins_per_octave = 24
@@ -12,17 +13,9 @@ sr = 32000.0  # in Hertz
 hop_duration = 0.016  # in seconds
 decision_duration = 2.048  # in seconds
 silence_threshold = -30 # in dB
-
 instrument_list = ['Cl', 'Co', 'Fh', 'Gt', 'Ob', 'Pn', 'Tr', 'Vl']
-solosDb8train_dir = '~/datasets/solosDb8/train'
-file_paths = di.symbolic.get_paths(solosDb8train_dir, instrument_list, 'wav')
-file_path = file_paths[0]
 
-(X_train, Y_train) = di.solosdb.get_XY(
-        file_paths,
-        instrument_list, decision_duration, fmin, hop_duration,
-        n_bins_per_octave, n_octaves, silence_threshold, sr)
-
+# Deep learning settings
 graph = di.learning.build_graph(
     X_height = 168,
     X_width = 128,
@@ -42,7 +35,16 @@ graph = di.learning.build_graph(
     dense2_channels = 8
 )
 
+# Compute audio features on training set
+solosDb8train_dir = '~/datasets/solosDb8/train'
+train_file_paths =
+    di.symbolic.get_paths(solosDb8train_dir, instrument_list, 'wav')
+(X_train, Y_train) = di.solosdb.get_XY(
+        train_file_paths,
+        instrument_list, decision_duration, fmin, hop_duration,
+        n_bins_per_octave, n_octaves, silence_threshold, sr)
+
+# Train deep network
 adagrad = keras.optimizers.Adagrad(lr=0.01, epsilon=1e-06)
 graph.compile(loss={'Y':'mean_squared_error'}, optimizer=adagrad)
 history = graph.fit({'X':X_train, 'Y':Y_train}, nb_epoch=1, batch_size=32)
-
