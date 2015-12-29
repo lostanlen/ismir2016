62938f747f22f002a0d66d7e5b4e61f1ae228212
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-29 11:31:47 +0100

    perceptual_qt returns the CQT of file as a matrix

--- DeepInstruments/audio.py
+++ DeepInstruments/audio.py
@@ -17,7 +17,6 @@ def perceptual_cqt(
     if y_sr != sr:
         y = librosa.resample(y, y_sr, sr)
     hop_length = hop_duration * sr
-    decision_length = decision_duration * sr / hop_length
     n_bins = n_octaves * n_bins_per_octave
     freqs = librosa.cqt_frequencies(
             bins_per_octave=n_bins_per_octave,
@@ -35,29 +34,12 @@ def perceptual_cqt(
             freqs,
             ref_power=1.0)
     n_hops = audio_features.shape[1]
+    decision_length = decision_duration * sr / hop_length
     if n_hops < decision_length:
         padding = np.zeros(n_bins, decision_length - n_hops)
         audio_features = np.hstack((audio_features, padding))
         n_hops = decision_length
-    n_windows = int(n_hops / decision_length)
-    n_hops_truncated = n_windows * decision_length
-    audio_features = np.transpose(audio_features)
-    audio_features = audio_features[:n_hops_truncated, :]
-    new_shape = (n_windows, decision_length, n_bins)
-    audio_features = np.reshape(audio_features, new_shape)
-    if len(y)<(n_windows * decision_duration * sr):
-        padding = np.zeros(n_windows * decision_duration * sr - len(y))
-        y = np.hstack((y, padding))
-    y_truncated = y[:n_windows * decision_duration * sr]
-    y_abs2 = y_truncated * y_truncated
-    y_abs2 = np.reshape(y_abs2, (n_windows, decision_duration * sr))
-    y_levels = np.sqrt(np.sum(y_abs2, axis=1))
-    y_levels /= np.max(y_levels)
-    threshold_lin = 10.0**(silence_threshold / 10.0)
-    window_bools = y_levels > threshold_lin
-    audio_features = audio_features[window_bools, :, :]
-    return np.transpose(audio_features, (0, 2, 1))
-
+    return np.transpose(audio_features)
 
 class ChunkGenerator(object):
     def __init__(self,
