156e2534f4e4500a4fb5976886aef47d9f49414d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-29 15:05:01 +0100

    typo in sandbox

--- DeepInstruments/sandbox.py
+++ DeepInstruments/sandbox.py
@@ -18,7 +18,7 @@ instrument_list = ['Cl', 'Co', 'Fh', 'Gt', 'Ob', 'Pn', 'Tr', 'Vl']
 # Compute audio features on training set
 solosDb8train_dir = '~/datasets/solosDb8/train'
 train_file_paths = di.symbolic.get_paths(solosDb8train_dir, instrument_list, 'wav')
-(X_sdbtrain_list, Ys_sdbtrain_list) = di.solosdb.get_XY(
+(X_sdbtrain_list, Y_sdbtrain_list) = di.solosdb.get_XY(
         train_file_paths,
         instrument_list, decision_duration, fmin, hop_duration,
         n_bins_per_octave, n_octaves, sr)
