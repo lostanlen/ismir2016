a53765fd4acf39f390fcf6d919173ed6bd8c2579
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-29 18:32:22 +0100

    def evaluate in learning.py

--- DeepInstruments/sandbox.py
+++ DeepInstruments/sandbox.py
@@ -19,7 +19,7 @@ instrument_list = ['Cl', 'Co', 'Fh', 'Gt', 'Ob', 'Pn', 'Tr', 'Vl']
 # Compute audio features on training set
 solosDb8train_dir = '~/datasets/solosDb8/train'
 train_file_paths = di.symbolic.get_paths(solosDb8train_dir, instrument_list, 'wav')
-(X_sdbtrain_list, Y_sdbtrain_list, Xtrain_mean, Xtrain_std) = di.solosdb.get_XY(
+(X_sdbtrain_list, Y_sdbtrain_list, X_train_mean, X_train_std) = di.solosdb.get_XY(
         train_file_paths,
         instrument_list, decision_duration, fmin, hop_duration,
         n_bins_per_octave, n_octaves, sr)
@@ -27,7 +27,7 @@ train_file_paths = di.symbolic.get_paths(solosDb8train_dir, instrument_list, 'wa
 # Compute audio features on test set
 solosDb8test_dir = '~/datasets/solosDb8/test'
 test_file_paths = di.symbolic.get_paths(solosDb8test_dir, instrument_list, 'wav')
-(X_sdbtest_list, Y_sdbtest_list) = di.solosdb.get_XY(
+(X_sdbtest_list, Y_sdbtest_list, X_test_mean, X_test_std) = di.solosdb.get_XY(
         test_file_paths,
         instrument_list, decision_duration, fmin, hop_duration,
         n_bins_per_octave, n_octaves, sr)
@@ -91,7 +91,6 @@ n_epochs = 100
 batch_size = 128
 epoch_size = 4096
 every_n_epoch = 5
-labels = range(len(instrument_list))
 
 
 for epoch_id in range(n_epochs):
@@ -101,48 +100,22 @@ for epoch_id in range(n_epochs):
         batch_size=batch_size,
         seed=None,
         epoch_size=epoch_size)
+
     print 'Epoch ', 1 + epoch_id
     progbar = keras.utils.generic_utils.Progbar(epoch_size)
     batch_id = 0
+
     for (X_batch, Y_batch) in dataflow:
         batch_id += 1
         loss = graph.train_on_batch({"X": X_batch, "Y": Y_batch})
         progbar.update(batch_id * batch_size)
 
-    if np.mod(epoch_id, every_n_epoch) == 0:
-        # Get training accuracy
-        dataflow = datagen.flow(
-            X_sdbtrain_list,
-            Y_sdbtrain_list,
-            batch_size=batch_size,
-            seed=None,
-            epoch_size=epoch_size)
-        y_train_true = np.zeros(epoch_size, dtype=int)
-        y_train_predicted = np.zeros(epoch_size, dtype=int)
-        batch_id = 0
-        for (X_batch, Y_batch) in dataflow:
-            Y_batch_predicted = graph.predict_on_batch({"X": X_batch})
-            Y_batch_predicted = np.hstack(Y_batch_predicted)
-            y_batch_predicted = np.argmax(Y_batch_predicted, axis=1)
-            batch_range = xrange(batch_id*batch_size, (batch_id+1)*batch_size)
-            y_train_predicted[batch_range] = y_batch_predicted
-            y_batch_true = np.argmax(Y_batch, axis=1)
-            y_train_true[batch_range] = y_batch_true
-            batch_id += 1
-
-        cm = sklearn.metrics.confusion_matrix(y_train_true, y_train_predicted, labels)
-        cm = cm.astype(np.float64)
-        cm = cm / cm.sum(axis=1)[:, np.newaxis]
-        train_accuracy = np.mean(np.diag(cm))
-        print "train accuracy = ", train_accuracy
-
-        # Get test accracy
-        test_prediction = graph.predict({"X": X_test})
-        Y_test_predicted = test_prediction["Y"]
-        y_test_predicted = np.argmax(Y_test_predicted, axis=1)
-        y_test_true = np.argmax(Y_test, axis=1)
-        cm = sklearn.metrics.confusion_matrix(y_test_true, y_test_predicted, labels)
-        cm = cm.astype(np.float64)
-        cm = cm / cm.sum(axis=1)[:, np.newaxis]
-        test_accuracy = np.mean(np.diag(cm))
-        print "test accuracy = ", test_accuracy
+    if np.mod(epoch_id+1, every_n_epoch) == 0:
+        di.learning.evaluate(
+                graph,
+                X_sdbtrain_list,
+                Y_sdbtrain_list,
+                X_test,
+                Y_test,
+                batch_size,
+                epoch_size)
