8b64688517667709896c5ac683450b3bf82d75a2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-29 12:10:18 +0100

    get_XY now returns (X_list, Y_list)

--- DeepInstruments/solosdb.py
+++ DeepInstruments/solosdb.py
@@ -12,38 +12,33 @@ def get_XY(
         hop_duration,
         n_bins_per_octave,
         n_octaves,
-        silence_threshold,
         sr):
     # Run perceptual CQT in parallel with joblib
     # n_jobs = -1 means that all CPUs are used
     memory = Memory(cachedir=os.path.expanduser('~/joblib'))
     cached_cqt = memory.cache(audio.perceptual_cqt, verbose=0)
-    file_cqts = Parallel(n_jobs=-1, verbose=20)(delayed(cached_cqt)(
+    X_files = Parallel(n_jobs=-1, verbose=20)(delayed(cached_cqt)(
             file_path,
             decision_duration,
             fmin,
             hop_duration,
             n_bins_per_octave,
             n_octaves,
-            silence_threshold,
             sr) for file_path in file_paths)
-    # Reduce all CQTs into one
-    X = np.vstack(file_cqts)
-    # Reshape to Theano-friendly format
-    new_shape = X.shape
-    new_shape = (new_shape[0], 1, new_shape[1], new_shape[2])
-    X = np.reshape(X, new_shape)
-    # Convert data to single precision
-    X = X.astype(np.float32)
-    # Normalize globally
-    X = X - np.mean(X)
-    X = X / np.var(X)
-    # Get instruments corresponding to all file paths
-    file_instruments = [symbolic.get_instrument(p, instrument_list) for p in
-                        file_paths]
-    # "Expand" the labels according to the number of windows per file
-    n_items_per_file = [cqt.shape[0] for cqt in file_cqts]
-    Y = symbolic.expand_instruments(file_instruments, n_items_per_file)
-    # Convert labels to single precision
-    Y = Y.astype(np.float32)
-    return (X, Y)
+    # Gather file CQTs according to each class
+    n_files = len(file_paths)
+    file_range = range(n_files)
+    n_instruments = len(instrument_list)
+    instr_range = range(n_instruments)
+    Y_files = [symbolic.get_instrument(p, instrument_list) for p in file_paths]
+    X_list = [np.hstack([X_files[f] for f in file_range if Y_files[f]==i]) for i in instr_range]
+    # Generate one-hot vectors for each class
+    Y_list = [np.zeros((1, n_instruments), dtype=np.float32) for i in instr_range]
+    for i in instr_range:
+        Y_list[i][0, i] = 1.0
+    # Standardize globally
+    X_global = np.hstack(X_files)
+    X_mean = np.mean(X_global)
+    X_var = np.std(X_global)
+    X_list = [(X-X_mean)/X_var for X in X_list]
+    return (X_list, Y_list)
