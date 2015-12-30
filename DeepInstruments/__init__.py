c85b61c04f331839af12940c74d661c4682cba1d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-30 19:57:26 +0100

    rename submodules

--- DeepInstruments/__init__.py
+++ DeepInstruments/__init__.py
@@ -1,5 +1,5 @@
 import DeepInstruments.audio
 import DeepInstruments.learning
-import DeepInstruments.rwc
-import DeepInstruments.solosdb
+import DeepInstruments.main
 import DeepInstruments.symbolic
+import DeepInstruments.wrangling
