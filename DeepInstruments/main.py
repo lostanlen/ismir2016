b9a1ebfe5914f380404f8db405f7ed25777d88b3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-30 19:57:34 +0100

    write main.py

---
+++ DeepInstruments/main.py
@@ -0,0 +1,94 @@
+import DeepInstruments as di
+import joblib
+import numpy as np
+import os
+
+memory = joblib.Memory(cachedir=os.path.expanduser('~/joblib'))
+cached_run = joblib.delayed(di.main.run)
+
+
+def run(batch_size,
+        decision_duration,
+        epoch_size,
+        every_n_epoch,
+        fmin,
+        hop_duration,
+        instrument_list,
+        n_bins_per_octave,
+        n_epochs,
+        n_octaves,
+        optimizer,
+        silence_threshold,
+        sr,
+        train_dirs,
+        test_dirs,
+        input_height,
+        input_width,
+        conv1_channels,
+        conv1_height,
+        conv1_width,
+        pool1_height,
+        pool1_width,
+        conv2_channels,
+        conv2_height,
+        conv2_width,
+        pool2_height,
+        pool2_width,
+        dense1_channels,
+        drop1_proportion,
+        dense2_channels,
+        drop2_proportion):
+    # Get training file paths
+    train_file_paths = \
+        di.symbolic.get_paths(train_dirs, instrument_list, 'wav')
+
+    # Compute features and labels in training set, list them per class
+    (X_train_list, Y_train_list) = \
+        di.wrangling.get_XY(train_file_paths, instrument_list,
+                            decision_duration, fmin, hop_duration,
+                            n_bins_per_octave, n_octaves, sr)
+
+    # Standardize globally the training set
+    X_global = np.hstack(X_train_list)
+    X_mean = np.mean(X_global)
+    X_std = np.std(X_global)
+    X_train_list = [(X-X_mean)/X_std for X in X_train_list]
+
+    # Build chunk generator
+    datagen = di.learning.ChunkGenerator(decision_duration, hop_duration,
+                                         silence_threshold)
+
+    # Compute features and labels in test set, list them per class
+    test_file_paths = \
+        di.symbolic.get_paths(test_dirs, instrument_list, 'wav')
+    (X_test_list, Y_test_list) = \
+        di.wrangling.get_XY(test_file_paths, instrument_list,
+                            decision_duration, fmin, hop_duration,
+                            n_bins_per_octave, n_octaves, sr)
+
+    # Standardize globally the test set according mean and std of training set
+    X_test_list = [(X-X_mean)/X_std for X in X_test_list]
+
+    # Format the test set into chunks, discard silent chunks
+    (X_test, Y_test) = di.wrangling.chunk_test_set(X_test_list, Y_test_list,
+                                                   hop_duration, sr)
+
+    # Build deep learning model as a Keras graph
+    graph = \
+        di.learning.build_graph(input_height, input_width,
+                                conv1_channels, conv1_height, conv1_width,
+                                pool1_height, pool1_width,
+                                conv2_channels, conv2_height, conv2_width,
+                                pool2_height, pool2_width,
+                                dense1_channels, drop1_proportion,
+                                dense2_channels, drop2_proportion,
+                                dense3_channels=len(instrument_list))
+
+    # Compile model
+    graph.compile(loss={"Y": "categorical_crossentropy"}, optimizer=optimizer)
+
+    # Train model, monitor accuracy on test set
+    report = di.learning.run_graph(X_train_list, Y_train_list, X_test, Y_test,
+                                   batch_size, datagen, epoch_size, graph)
+
+    return report
