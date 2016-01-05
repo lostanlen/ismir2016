735d5917ed40048138b36605fd9d7de1b92edd86
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-05 18:57:12 +0100

    start basic instrument list

--- DeepInstruments/sandbox.py
+++ DeepInstruments/sandbox.py
@@ -2,3 +2,9 @@ import os
 os.environ["MEDLEYDB_PATH"] = os.path.expanduser("~/datasets/MedleyDB")
 import medleydb
 
+
+instrument_list = ['female_singer', 'acoustic_guitar', 'violin']
+mtrack_list = medleydb.load_all_multitracks()
+
+for track in mtrack_list:
+    print([s.instrument for s in track.stems])
