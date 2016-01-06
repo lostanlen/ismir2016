fd83a2291d3c6289ae0d02b9b440e2bb40de31e6
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-06 21:33:31 +0100

    better names in get_activations

--- DeepInstruments/wrangling.py
+++ DeepInstruments/wrangling.py
@@ -42,17 +42,17 @@ def get_XY(file_paths,
     return (X_list, Y_list)
 
 
-def get_activations(instrument_names, track):
-    instrument_matches = di.wrangling.instrument_stems(instrument_names, track)
+def get_activations(names, track):
+    instrument_matches = di.wrangling.instrument_stems(names, track)
     stem_activations = track.activations_data
     stem_activations = np.vstack(stem_activations)[:, 1:]
     n_frames = stem_activations.shape[0]
     n_instruments = len(instrument_matches)
     activations = np.zeros((n_frames, n_instruments), dtype=np.float32)
     for instrument_index in range(n_instruments):
-        instrument_stems = instrument_matches[instrument_index]
-        if instrument_stems:
-            instrument_stem_activations = stem_activations[:, instrument_stems]
+        stems = instrument_matches[instrument_index]
+        if stems:
+            instrument_stem_activations = stem_activations[:, stems]
             activations[:, instrument_index] = \
                 np.max(instrument_stem_activations, axis=1)
     return activations
