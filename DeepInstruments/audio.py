c512f4d229f43166dd7dca2b1a938f5fe3d12296
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-06 09:34:59 +0100

    pad waveform, not CQT, in get_X

--- DeepInstruments/audio.py
+++ DeepInstruments/audio.py
@@ -1,9 +1,6 @@
 import librosa
 import numpy as np
 
-sr, X_stereo = track.audio_data
-X_stereo = X_stereo.astype(np.float32)
-X_mono = np.sum(X_stereo, axis=1) / (32768.0 * 2)
 
 def get_X(
         file_path,
@@ -13,12 +10,13 @@ def get_X(
         n_bins_per_octave,
         n_octaves,
         track):
-    x_stereo = track.audio_data
+    (sr, x_stereo) = track.audio_data
     x_stereo = x_stereo.astype(np.float32)
     x_mono = np.sum(x_stereo, axis=1) / (32768.0 * 2)
     if x_mono.shape[0] < (decision_duration * sr):
-        padding = np.zeros(x_mono.shape[0] - decision_duration * sr)
-        x_mono =
+        padding_length = x_mono.shape[0] - decision_duration * sr
+        padding = np.zeros(padding_length, dtype=np.float32)
+        x_mono = np.hstack((x_mono, padding))
     n_bins = n_octaves * n_bins_per_octave
     hop_length = hop_duration * sr
     freqs = librosa.cqt_frequencies(
@@ -36,10 +34,5 @@ def get_X(
             CQT ** 2,
             freqs,
             ref_power=1.0)
-    n_hops = X.shape[1]
-    decision_length = int(decision_duration * sr / hop_length)
-    if n_hops < decision_length:
-        padding = np.zeros(n_bins, decision_length - n_hops)
-        X = np.hstack((X, padding))
     X = X.astype(np.float32)
     return X
