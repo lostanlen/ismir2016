c2b649f53aece1c961fbbcc362ceb940df7247c8
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-14 09:46:27 +0100

    update train_accuracy function

--- DeepInstruments/singlelabel.py
+++ DeepInstruments/singlelabel.py
@@ -277,46 +277,28 @@ def split_stems(names,
     return test_stems, training_stems
 
 
-def test_accuracy(X_test, Y_test, batch_size, epoch_size, graph):
-    labels = range(Y_test.shape[1])
-    test_prediction = graph.predict({"X": X_test})
-    Y_test_predicted = test_prediction["Y"]
-    y_test_predicted = np.argmax(Y_test_predicted, axis=1)
-    y_test_true = np.argmax(Y_test, axis=1)
-    cm = sklearn.metrics.confusion_matrix(y_test_true, y_test_predicted,
-                                          labels)
-    cm = cm.astype(np.float64)
-    cm = cm / cm.sum(axis=1)[:, np.newaxis]
-    test_accuracies = np.diag(cm)
-    mean_accuracy = np.mean(test_accuracies)
-    std_accuracy = np.std(test_accuracies)
-    print "test mean accuracy = ", mean_accuracy, " +/- ", std_accuracy
-    return test_accuracies
-
-
-def train_accuracy(X_train_list, Y_train_list,
-                   batch_size, datagen, epoch_size, graph):
-    labels = len(Y_train_list)
-    dataflow = datagen.flow(X_train_list, Y_train_list,
-        batch_size=batch_size,
-        epoch_size=epoch_size)
-    y_train_true = np.zeros(epoch_size, dtype=int)
-    y_train_predicted = np.zeros(epoch_size, dtype=int)
+def train_accuracy(batch_size, datagen, epoch_size, graph):
+    n_batches = int(math.ceil(float(epoch_size) / batch_size))
+    n_instruments = datagen.Y[0][0].shape[0]
+    dataflow = datagen.flow(batch_size=batch_size, epoch_size=epoch_size)
+    y_train_predicted = np.zeros((n_batches, batch_size), dtype=int)
+    y_train_true = np.zeros((n_batches, batch_size), dtype=int)
     batch_id = 0
     for (X_batch, Y_batch) in dataflow:
         Y_batch_predicted = graph.predict_on_batch({"X": X_batch})
         Y_batch_predicted = np.hstack(Y_batch_predicted)
         y_batch_predicted = np.argmax(Y_batch_predicted, axis=1)
-        batch_range = xrange(batch_id*batch_size, (batch_id+1)*batch_size)
-        y_train_predicted[batch_range] = y_batch_predicted
+        y_train_predicted[batch_id, :] = y_batch_predicted
         y_batch_true = np.argmax(Y_batch, axis=1)
-        y_train_true[batch_range] = y_batch_true
+        y_train_true[batch_id, :] = y_batch_true
         batch_id += 1
+    y_train_predicted = np.ndarray.flatten(y_train_predicted)
+    y_train_true = np.ndarray.flatten(y_train_true)
     cm = sklearn.metrics.confusion_matrix(y_train_true, y_train_predicted,
-                                          labels)
+                                          range(n_instruments))
     cm = cm.astype(np.float64)
     cm = cm / cm.sum(axis=1)[:, np.newaxis]
-    train_accuracies = np.diag(cm)
+    train_accuracies = 100 * np.diag(cm)
     mean_accuracy = np.mean(train_accuracies)
     std_accuracy = np.std(train_accuracies)
     print "train accuracy = ", mean_accuracy, " +/- ", std_accuracy
