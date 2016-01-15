c7e2551be942fd0082af4e10fc0738015c48c0ae
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-15 16:31:15 +0100

    start writing descriptors.py

---
+++ DeepInstruments/descriptors.py
@@ -0,0 +1,7 @@
+import DeepInstruments as di
+import os
+
+mdbsi_path = os.path.join(os.path.expanduser("~"),
+                          "datasets", "medleydb-single-instruments")
+
+os.listdir(mdbsi_path)
