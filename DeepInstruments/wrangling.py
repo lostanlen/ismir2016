4ea24a84771c6a33121b95d7cd80a8f9fc407a6d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-15 09:49:11 +0100

    bugfix wrangling.py

--- DeepInstruments/wrangling.py
+++ DeepInstruments/wrangling.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import shutil
 
+
 def chunk_waveforms(dataset_path,
                     decision_hop,
                     decision_length,
@@ -12,16 +13,14 @@ def chunk_waveforms(dataset_path,
     (test_stems, training_stems) = di.singlelabel.split_stems(
         di.singlelabel.names, di.singlelabel.test_discarded,
         di.singlelabel.training_discarded, di.singlelabel.training_to_test)
-    print(training_or_test)
     if training_or_test == "training":
         stems = training_stems
     elif training_or_test == "test":
         stems = test_stems
     else:
         raise ValueError("Input to chunk_waveforms must be training or test")
-    print(stems)
     for instrument_id in range(len(stems)):
-        instrument_name = training_stems[instrument_id][0].instrument.name
+        instrument_name = stems[instrument_id][0].instrument.name
         instrument_id_str = repr('%(i)02d' % {"i": instrument_id})[1:-1]
         instrument_folder = instrument_id_str + "_" + instrument_name
         instrument_folder_path = os.path.join(dataset_path,
@@ -32,9 +31,9 @@ def chunk_waveforms(dataset_path,
         except OSError:
             shutil.rmtree(instrument_folder_path)
             os.makedirs(instrument_folder_path)
-        for file_id in range(len(training_stems[instrument_id])):
+        for file_id in range(len(stems[instrument_id])):
             file_id_str = repr('%(i)02d' % {"i": file_id})[1:-1]
-            stem = training_stems[instrument_id][file_id]
+            stem = stems[instrument_id][file_id]
             track_name = stem.track.name
             stem_name = stem.name
             file_folder = file_id_str + "_" + track_name + "_" + stem_name
