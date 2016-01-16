8ac135f216164130abf6e93be1ad5f7232414bb6
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-16 21:41:30 +0100

    get all paths of chunk waveforms

--- DeepInstruments/descriptors.py
+++ DeepInstruments/descriptors.py
@@ -1,7 +1,17 @@
 import DeepInstruments as di
+import fnmatch
 import os
 
-mdbsi_path = os.path.join(os.path.expanduser("~"),
-                          "datasets", "medleydb-single-instruments")
-
-os.listdir(mdbsi_path)
+training_path = os.path.join(os.path.expanduser("~"),
+                             "datasets",
+                             "medleydb-single-instruments",
+                             "training")
+set_path = training_path
+class_names = os.listdir(set_path)
+chunk_paths = []
+for class_name in class_names:
+    class_path = os.path.join(set_path, class_name)
+    paths = [os.path.join(path, name)
+             for (path, subdir, names) in os.walk(class_path)
+             for name in names]
+    chunk_paths.append(paths)
