d22bd6e16f4c6da31e82e943dd7476f1586607c4
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-17 12:33:09 +0100

    compute and stack audio descriptors

--- DeepInstruments/descriptors.py
+++ DeepInstruments/descriptors.py
@@ -1,5 +1,6 @@
 import DeepInstruments as di
-import fnmatch
+import librosa
+import numpy as np
 import os
 
 training_path = os.path.join(os.path.expanduser("~"),
@@ -7,11 +8,48 @@ training_path = os.path.join(os.path.expanduser("~"),
                              "medleydb-single-instruments",
                              "training")
 set_path = training_path
-class_names = os.listdir(set_path)
-chunk_paths = []
-for class_name in class_names:
-    class_path = os.path.join(set_path, class_name)
-    paths = [os.path.join(path, name)
-             for (path, subdir, names) in os.walk(class_path)
-             for name in names]
-    chunk_paths.append(paths)
+
+chunk_paths = [
+    [os.path.join(path, name)
+     for (path, subdir, names) in os.walk(os.path.join(set_path, class_name))
+     for name in names]
+    for class_name in os.listdir(set_path)]
+
+chunk_path = chunk_paths[0][0]
+waveform, sr = librosa.core.load(chunk_path)
+mfcc = librosa.feature.mfcc(waveform, sr)
+delta_mfcc = librosa.feature.delta(mfcc)
+deltadelta_mfcc = librosa.feature.delta(mfcc, order=2)
+mean_mfcc = np.mean(mfcc, axis=-1)
+mean_delta_mfcc = np.mean(delta_mfcc, axis=1)
+mean_deltadelta_mfcc = np.mean(deltadelta_mfcc, axis=1)
+bandwidth = librosa.feature.spectral_bandwidth(waveform, sr)
+mean_bandwidth = np.mean(bandwidth, axis=1)
+std_bandwidth = np.std(bandwidth, axis=1)
+centroid = librosa.feature.spectral_centroid(waveform, sr)
+mean_centroid = np.mean(centroid, axis=1)
+std_centroid = np.std(centroid, axis=1)
+contrast = librosa.feature.spectral_contrast(waveform, sr)
+mean_contrast = np.mean(contrast, axis=1)
+std_contrast = np.std(contrast, axis=1)
+rolloff = librosa.feature.spectral_rolloff(waveform, sr)
+mean_rolloff = np.mean(rolloff, axis=1)
+std_rolloff = np.std(rolloff, axis=1)
+zcr = librosa.feature.zero_crossing_rate(y, sr)
+mean_zcr = np.mean(zcr)
+std_zcr = np.std(zcr)
+descriptors = np.hstack(
+        (mean_mfcc,
+         mean_delta_mfcc,
+         mean_deltadelta_mfcc,
+         mean_bandwidth,
+         std_bandwidth,
+         mean_centroid,
+         std_centroid,
+         mean_contrast,
+         std_contrast,
+         mean_rolloff,
+         std_rolloff,
+         mean_zcr,
+         std_zcr)
+)
