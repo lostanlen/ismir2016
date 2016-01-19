f0fa195758f8a8300d1c8d7735c92fcdbfc5d6f2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-19 17:20:29 +0100

    update descriptors

--- DeepInstruments/descriptors.py
+++ DeepInstruments/descriptors.py
@@ -21,10 +21,8 @@ def get_paths(training_or_test):
 
 
 def get_X(paths):
-    X = joblib.Parallel(n_jobs=-1, verbose=10)(
-            joblib.delayed(di.descriptors.cached_get_descriptors)(path)
-            for path in paths
-    )
+    X = [joblib.delayed(di.descriptors.cached_get_descriptors)(path)
+         for path in paths]
     return np.vstack(X)
 
 
