f118adf4854fa8071dd620a3077d7243e4bffc3f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-28 20:00:55 +0100

    permute and collapse layers

--- DeepInstruments/sandbox.py
+++ DeepInstruments/sandbox.py
@@ -8,7 +8,9 @@ from keras.models import Graph
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.core import Merge
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Convolution2D, MaxPooling1D,\
+                                       MaxPooling2D
+from keras.layers.core import Permute
 import math
 import random
 
@@ -19,11 +21,13 @@ datagen = di.singlelabel.ScalogramGenerator(decision_length, fmin,
                                             n_octaves, training_stems)
 
 
-conv1_channels = 10
-conv1_height = 3
-conv1_width = 3
 X_height = 96
 X_width = 128
+conv1_channels = 2
+conv1_height = 3
+conv1_width = 3
+pool1_height = 3
+pool1_width = 3
 
 graph = Graph()
 
@@ -43,13 +47,22 @@ graph.add_node(relu1, name="relu1", input="conv1")
 pool1_X = MaxPooling2D(pool_size=(pool1_height, pool1_width))
 graph.add_node(pool1_X, name="pool1_X", input="relu1")
 
+permuted_X = Permute(dims=(2, 3, 1))
+graph.add_node(permuted_X, name="permuted", input="pool1_X")
+
+collapsed = MaxPooling1D(pool_length=conv1_channels)
+graph.add_node(collapsed, name="collapsed", input="permuted")
+
 # Layers towards pitch
-# pool1_Z = MaxPooling2D(pool_size=(pool1_height, pool1_width))
-# graph.add_node(pool1_Z, name="pool1_Z", input="Z")
+pool1_Z = MaxPooling2D(pool_size=(pool1_height, pool1_width))
+graph.add_node(pool1_Z, name="pool1_Z", input="Z")
+
+permuted_Z = Permute(dims=(2, 3, 1))
+graph.add_node(permuted_X, name="permuted")
 #
 # pool1_G = MaxPooling2D(pool_size=(pool1_height, pool1_width))
 # graph.add_node(pool1_G, name="pool1_G", input="G")
 
-graph.add_output(name='masked_Z', input='pool1_X')
+graph.add_output(name='out', input='collapsed')
 
-graph.compile(loss={'masked_Z': 'mse'}, optimizer="sgd")
+graph.compile(loss={'out': 'mse'}, optimizer="sgd")
