1b4b016ccaca612d47b353ed69e02a499b2926c7
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-11 14:54:00 +0100

    call fit_generator from the sandbox

--- DeepInstruments/sandbox.py
+++ DeepInstruments/sandbox.py
@@ -2,33 +2,43 @@ import DeepInstruments as di
 import numpy as np
 from keras.utils.generic_utils import Progbar
 
-X_height = 96
-X_width = 128
-conv1_channels = 80
+# Parameters for audio
+decision_length = 131072  # in samples
+fmin = 55  # in Hz
+hop_length = 1024  # in samples
+n_bins_per_octave = 12
+n_octaves = 8
+
+# Parameters for ConvNet
+conv1_channels = 40
 conv1_height = 36
-conv1_width = 8
-pool1_height = 6
-pool1_width = 8
+conv1_width = 4
+pool1_height = 4
+pool1_width = 4
 conv2_channels = 40
-conv2_height = 4
-conv2_width = 16
-pool2_height = 4
+conv2_height = 12
+conv2_width = 8
+pool2_height = 6
 pool2_width = 16
 drop1_proportion = 0.5
 dense1_channels = 64
 drop2_proportion = 0.5
+
+# Parameters for learning
+batch_size = 128
+n_epochs = 10
+optimizer = "adam"
+samples_per_epoch = 8192
+mask_weight = 1.0e3
+
+# I/O sizes
+X_height = n_bins_per_octave * n_octaves
+X_width = decision_length / hop_length
+mask_height = X_height / pool1_height
+mask_width = X_width / pool1_width
 dense2_channels = 8
 
-batch_size = 128
-decision_length = 131072  # in samples
-epoch_size = 8192
-fmin = 55  # in Hz
-hop_length = 1024  # in samples
-n_bins_per_octave = 12
-n_epochs = 20
-n_octaves = 8
-optimizer = "adam"
-
+# Build ConvNet as a Keras graph, compile it with Theano
 graph = di.learning.build_graph(
     X_height,
     X_width,
@@ -46,31 +56,34 @@ graph = di.learning.build_graph(
     dense1_channels,
     drop2_proportion,
     dense2_channels)
-
-from keras.utils.visualize_util import plot
-plot(graph, to_file='graph.png')
-
 graph.compile(loss={"Y": "categorical_crossentropy",
                     "zero": "mse"}, optimizer=optimizer)
 
+# Get single-label split (MedleyDB for training, solosDb for test
 (test_stems, training_stems) = di.singlelabel.get_stems()
 
+# Compute audio features and retrieve melodies on the training set
 datagen = di.singlelabel.ScalogramGenerator(
-        decision_length, fmin, hop_length, n_bins_per_octave, n_octaves,
+        decision_length, fmin, hop_length, mask_weight,
+        n_bins_per_octave, n_octaves,
         training_stems)
 
+# Compute audio features on the test set
 test_paths = di.singlelabel.get_paths("test")
 X_test = datagen.get_X(test_paths)
 y_test = np.hstack(map(di.descriptors.get_y, test_paths))
 
+
+# Train ConvNet
+graph.fit_generator(datagen, samples_per_epoch, n_epochs, verbose=1)
+
 from keras.utils.generic_utils import Progbar
 mean_training_loss_history = []
-dataflow = datagen.flow(batch_size=batch_size, epoch_size=epoch_size)
 batch_losses = np.zeros(epoch_size / batch_size)
 
 for epoch_id in xrange(n_epochs):
     dataflow = datagen.flow(batch_size=batch_size, epoch_size=epoch_size)
-    print 'Epoch ', 1 + epoch_id
+    print "Epoch ", 1 + epoch_id
     progbar = Progbar(epoch_size)
     batch_id = 0
     for (X_batch, Y_batch, Z_batch, G_batch) in dataflow:
