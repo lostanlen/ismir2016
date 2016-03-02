1b5361d0a44bb3483366e7d7c5f7a184503ba1f0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-02 13:30:34 +0100

    leaky relu's in spiral convnet

--- DeepInstruments/spiral.py
+++ DeepInstruments/spiral.py
@@ -40,40 +40,46 @@ def build_graph(
 
     # Octave-wise convolutional layers
     conv1_X0 = Convolution2D(conv1_channels, conv1_height, conv1_width,
-                             border_mode="valid", activation="relu")
+                             border_mode="valid")
     graph.add_node(conv1_X0, name="conv1_X0", input="X0")
     conv1_X1 = Convolution2D(conv1_channels, conv1_height, conv1_width,
-                             border_mode="valid", activation="relu")
+                             border_mode="valid")
     graph.add_node(conv1_X1, name="conv1_X1", input="X1")
     conv1_X2 = Convolution2D(conv1_channels, conv1_height, conv1_width,
-                             border_mode="valid", activation="relu")
+                             border_mode="valid")
     graph.add_node(conv1_X2, name="conv1_X2", input="X2")
     conv1_X3 = Convolution2D(conv1_channels, conv1_height, conv1_width,
-                             border_mode="valid", activation="relu")
+                             border_mode="valid")
     graph.add_node(conv1_X3, name="conv1_X3", input="X3")
     conv1_X4 = Convolution2D(conv1_channels, conv1_height, conv1_width,
                              border_mode="valid", activation="relu")
     graph.add_node(conv1_X4, name="conv1_X4", input="X4")
     conv1_X5 = Convolution2D(conv1_channels, conv1_height, conv1_width,
-                             border_mode="valid", activation="relu")
+                             border_mode="valid")
     graph.add_node(conv1_X5, name="conv1_X5", input="X5")
     conv1_X6 = Convolution2D(conv1_channels, conv1_height, conv1_width,
-                             border_mode="valid", activation="relu")
+                             border_mode="valid")
     graph.add_node(conv1_X6, name="conv1_X6", input="X6")
 
     # Spiral concatenation and pooling
-    pool1_X = MaxPooling2D(pool_size=(pool1_height, pool1_width))
-    graph.add_node(pool1_X, name="pool1_X",
+    relu1 = LeakyReLU()
+    graph.add_node(relu1, name="relu1",
                    inputs=["conv1_X0", "conv1_X1", "conv1_X2", "conv1_X3",
                            "conv1_X4", "conv1_X5", "conv1_X6"], concat_axis=2)
 
+    pool1_X = MaxPooling2D(pool_size=(pool1_height, pool1_width))
+    graph.add_node(pool1_X, name="pool1_X", input="relu1")
+
     # Time-frequency convolutions
     conv2 = Convolution2D(conv2_channels, conv2_height, conv2_width,
-                          border_mode="valid", activation="relu")
+                          border_mode="valid")
     graph.add_node(conv2, name="conv2", input="pool1_X")
 
+    relu2 = LeakyReLU()
+    graph.add_node(relu2, name="relu2", input="conv2")
+
     pool2 = MaxPooling2D(pool_size=(pool2_height, pool2_width))
-    graph.add_node(pool2, name="pool2", input="conv2")
+    graph.add_node(pool2, name="pool2", input="relu2")
 
     # Multi-layer perceptron with dropout
     flatten = Flatten()
@@ -82,11 +88,14 @@ def build_graph(
     drop1 = Dropout(drop1_proportion)
     graph.add_node(drop1, name="drop1", input="flatten")
 
-    dense1 = Dense(dense1_channels, activation="relu")
+    dense1 = Dense(dense1_channels)
     graph.add_node(dense1, name="dense1", input="drop1")
 
+    relu3 = LeakyReLU()
+    graph.add_node(relu3, name="relu3", input="dense1")
+
     drop2 = Dropout(drop2_proportion)
-    graph.add_node(drop2, name="drop2", input="dense1")
+    graph.add_node(drop2, name="drop2", input="relu3")
 
     dense2 = Dense(dense2_channels, activation="softmax")
     graph.add_node(dense2, name="dense2", input="drop2")
