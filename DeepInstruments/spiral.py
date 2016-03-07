0d7fe3a593658d9f5ae4ab485be2098c6af4f922
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-07 15:07:46 +0100

    update build graph

--- DeepInstruments/spiral.py
+++ DeepInstruments/spiral.py
@@ -33,7 +33,7 @@ def build_graph(
 
     # Input
     X_height = n_octaves * n_bins_per_octave
-    for octave_index in range(1, n_octaves - 2):
+    for octave_index in range(0, n_octaves - 1):
         name = "X" + str(octave_index)
         graph.add_input(name=name, input_shape=(1, X_height, X_width))
 
