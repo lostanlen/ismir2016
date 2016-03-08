f950ffe75b3d2b94f082b1de473d9b937d6e8d87
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-08 10:14:42 +0100

    only keep X_bn

--- DeepInstruments/scalogram.py
+++ DeepInstruments/scalogram.py
@@ -48,11 +48,8 @@ def build_graph(
                           border_mode="valid", init=init)
     graph.add_node(conv1, name="conv1", input="X_bn")
 
-    bn1 = BatchNormalization(mode=1)
-    graph.add_node(bn1, name="bn1", input="conv1")
-
     relu1 = LeakyReLU()
-    graph.add_node(relu1, name="relu1", input="bn1")
+    graph.add_node(relu1, name="relu1", input="conv1")
 
     pool1_X = MaxPooling2D(pool_size=(pool1_height, pool1_width))
     graph.add_node(pool1_X, name="pool1_X", input="relu1")
@@ -62,11 +59,8 @@ def build_graph(
                           border_mode="valid", init=init)
     graph.add_node(conv2, name="conv2", input="pool1_X")
 
-    bn2 = BatchNormalization(mode=1)
-    graph.add_node(bn2, name="bn2", input="conv2")
-
     relu2 = LeakyReLU()
-    graph.add_node(relu2, name="relu2", input="bn2")
+    graph.add_node(relu2, name="relu2", input="conv2")
 
     pool2 = MaxPooling2D(pool_size=(pool2_height, pool2_width))
     graph.add_node(pool2, name="pool2", input="relu2")
@@ -79,9 +73,6 @@ def build_graph(
 
     dense1 = Dense(dense1_channels, init="lecun_uniform")
     graph.add_node(dense1, name="dense1", input="drop1")
-
-    bn3 = BatchNormalization(mode=0)
-    graph.add_node(bn3, name="bn3", input="dense1")
 
     relu3 = LeakyReLU()
     graph.add_node(relu3, name="relu3", input="bn3")
