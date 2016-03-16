3fda7bf20e593873745b21e29a47d7598a850ec0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-16 17:29:32 +0100

    init to X0 and X1

--- DeepInstruments/spiral.py
+++ DeepInstruments/spiral.py
@@ -32,9 +32,8 @@ def build_graph(
 
     # Input
     X_height = n_octaves * n_bins_per_octave
-    for octave_index in range(0, n_octaves - 2):
-        name = "X" + str(octave_index)
-        graph.add_input(name=name, input_shape=(1, X_height, X_width))
+    graph.add_input(name="X0", input_shape=(1, X_height, X_width))
+    graph.add_input(name="X1", input_shape=(1, X_height, X_width))
 
     # Octave-wise convolutional layers
     init = "he_normal"
