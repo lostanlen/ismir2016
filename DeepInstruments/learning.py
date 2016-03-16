6a733eff3fa9bd2c5d1e3eeb27fa467cc7752ae1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-16 09:17:19 +0100

    up

--- DeepInstruments/learning.py
+++ DeepInstruments/learning.py
@@ -45,8 +45,8 @@ def build_graph(
 def predict(graph, is_spiral, X_test, offset):
     if is_spiral:
         Q = 12
-        X0 = X_test * window(X_test, 0, 4*Q, 3*Q) - offset / 2
-        X1 = X_test * window(X_test, 3*Q+1, 5*Q, 4*Q) - offset / 2
+        X0 = X_batch * window(X_batch, 0, 5*Q, top_width=3*Q) - offset / 2
+        X1 = X_batch * window(X_batch, 4*Q, 4*Q, top_width=2*Q) - offset / 2
         class_probs = graph.predict({"X0": X0, "X1": X1})["Y"]
     else:
         X = X_test - offset
@@ -61,8 +61,8 @@ def substract_and_mask(args):
 def train_on_batch(graph, is_spiral, X_batch, Y_batch, offset):
     if is_spiral:
         Q = 12
-        X0 = X_batch * window(X_batch, 0, 5*Q, 1*Q) - offset / 2
-        X1 = X_batch * window(X_batch, 3*Q, 5*Q, 4*Q) - offset / 2
+        X0 = X_batch * window(X_batch, 0, 5*Q, top_width=3*Q) - offset / 2
+        X1 = X_batch * window(X_batch, 4*Q, 4*Q, top_width=2*Q) - offset / 2
         loss = graph.train_on_batch({"X0": X0, "X1": X1, "Y": Y_batch})
         return loss
     else:
@@ -74,7 +74,7 @@ def train_on_batch(graph, is_spiral, X_batch, Y_batch, offset):
 def window(X_batch, start, full_width, top_width=None):
     if not top_width:
         top_width = full_width / 3
-    alpha = float(top_width) / full_width
+    alpha = 1.0 - float(top_width) / full_width
     phi = np.zeros((1, 1, X_batch.shape[2], 1))
     support = xrange(start, start + full_width)
     phi[0, 0, support, 0] = scipy.signal.tukey(full_width, alpha=alpha)
