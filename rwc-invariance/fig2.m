8b8fd8a8d5c0b504780145d869b6d35a05fc8999
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-20 20:05:42 +0100

    update fig2.m

--- rwc-invariance/fig2.m
+++ rwc-invariance/fig2.m
@@ -11,7 +11,7 @@ file_metas = parse_rwc(rwc_path);
 nBatches = length(unique([file_metas.batch_id]));
 
 instrument_names = {'Clarinet', 'Flute', 'Trumpet', 'Piano', ...
-    'Tenor Saxophone', 'Piano', 'Violin'};
+    'Tenor Saxophone', 'Violin'};
 batches = intersect( ...
     unique([file_metas(ismember({file_metas.instrument_name}, ...
     instrument_names)).batch_id]), ...
@@ -37,37 +37,103 @@ features = load_features(setting, batches, 'max')
 percentiles = [0.1 0.25 0.5 0.75 0.9] * 100;
 nPercentiles = length(percentiles);
 
-instruments = unique([features.instrument_id]);
-nInstruments = length(instruments);
+nInstruments = length(instrument_names);
 instrument_dists = cell(1, nInstruments);
+instruments = zeros(1, nInstruments);
 for instrument_id = 1:nInstruments
+    instrument_name = instrument_names(instrument_id);
+    instruments(instrument_id) = ...
+        unique([file_metas(strcmp({file_metas.instrument_name}, ...
+        instrument_name)).instrument_id]);
     instrument = instruments(instrument_id);
     in_cluster = [features([features.instrument_id]==instrument).data].';
     out_cluster = [features([features.instrument_id]~=instrument).data].';
     instrument_dists{instrument_id} = ...
-        pdist2(in_cluster, out_cluster, 'euclidean');
+        pdist2(in_cluster, out_cluster, 'euclidean').^2;
 end
 all_instrument_dists = [instrument_dists{:}];
 all_instrument_dists = all_instrument_dists(:);
 hist(all_instrument_dists, 1000);
 
-%%
-pitch_dists = cell(1, nInstruments);
-all_pitch_dists = cell(1, nInstruments);
+
+%% Measure distances within instruments, with different pitch, nuance and style
+note_dists = cell(1, nInstruments);
 for instrument_id = 1:nInstruments
     instrument = instruments(instrument_id);
-    instrument_features = features([features.instrument_id]==instrument);
-    pitches = unique([instrument_features.pitch_id]);
-    nPitches = length(pitches)
-    pitch_dists{instrument_id} = cell(1, nPitches);
-    for pitch_id = 1:nPitches
-        pitch = pitches(pitch_id);
-        in_cluster = ...
-            [instrument_features([instrument_features.pitch_id]==pitch).data].';
-        out_cluster = ...
-            [instrument_features([instrument_features.pitch_id]~=pitch).data].';
-        pitch_dists{instrument_id}{pitch_id} = ...
-            pdist2(in_cluster, out_cluster, 'euclidean');
+    instrument_features = features([features.instrument_id] == instrument);
+    nNotes = length(instrument_features);
+    pitches = [instrument_features.pitch_id];
+    nuances = [instrument_features.nuance_id];
+    styles = [instrument_features.style_id];
+    a_distances = cell(1, nNotes);
+    for a_index = 1:nNotes
+        a = instrument_features(a_index);
+        bs = instrument_features( ...
+            (pitches ~= a.pitch_id) & ...
+            (nuances ~= a.nuance_id) & ...
+            (styles ~= a.style_id));
+        a_distances{a_index} = pdist2(a.data.', [bs.data].').^2;
     end
-    all_pitch_dists = [pitch_dists{instrument_id}{:}];
+    note_dists{instrument_id} = [a_distances{:}];
 end
+
+
+%% Measure distances within instrument, between notes of same pitch
+pitch_dists = cell(1, nInstruments);
+for instrument_id = 1:nInstruments
+    instrument = instruments(instrument_id);
+    instrument_features = features([features.instrument_id] == instrument);
+    nNotes = length(instrument_features);
+    pitches = [instrument_features.pitch_id];
+    nuances = [instrument_features.nuance_id];
+    styles = [instrument_features.style_id];
+    a_distances = cell(1, nNotes);
+    for a_index = 1:nNotes
+        a = instrument_features(a_index);
+        bs = instrument_features(pitches == a.pitch_id);
+        a_distances{a_index} = pdist2(a.data.', [bs.data].').^2;
+    end
+    pitch_dists{instrument_id} = [a_distances{:}];
+end
+
+%% Same nuance
+nuance_dists = cell(1, nInstruments);
+for instrument_id = 1:nInstruments
+    instrument = instruments(instrument_id);
+    instrument_features = features([features.instrument_id] == instrument);
+    nNotes = length(instrument_features);
+    pitches = [instrument_features.pitch_id];
+    nuances = [instrument_features.nuance_id];
+    styles = [instrument_features.style_id];
+    a_distances = cell(1, nNotes);
+    for a_index = 1:nNotes
+        a = instrument_features(a_index);
+        bs = instrument_features(nuances == a.nuance_id);
+        a_distances{a_index} = pdist2(a.data.', [bs.data].').^2;
+    end
+    nuance_dists{instrument_id} = [a_distances{:}];
+end
+
+%% Same style
+style_dists = cell(1, nInstruments);
+for instrument_id = 1:nInstruments
+    instrument = instruments(instrument_id);
+    instrument_features = features([features.instrument_id] == instrument);
+    nNotes = length(instrument_features);
+    pitches = [instrument_features.pitch_id];
+    nuances = [instrument_features.nuance_id];
+    styles = [instrument_features.style_id];
+    a_distances = cell(1, nNotes);
+    for a_index = 1:nNotes
+        a = instrument_features(a_index);
+        bs = instrument_features(styles == a.style_id);
+        a_distances{a_index} = pdist2(a.data.', [bs.data].').^2;
+    end
+    style_dists{instrument_id} = [a_distances{:}];
+end
+
+%%
+cellfun(@median, note_dists)
+cellfun(@median, style_dists)
+cellfun(@median, nuance_dists)
+cellfun(@median, pitch_dists)
