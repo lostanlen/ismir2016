249a14a67d57d63c899355aa517166d801d8e2f6
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-26 08:35:46 +0100

    here come the standard deviations

--- research/Table2.py
+++ research/Table2.py
@@ -12,25 +12,18 @@ n_trials = file_report.__len__()
 
 loss_tensor = np.zeros((n_archs, 1, n_trials))
 chunk_tensor = np.zeros((n_archs, n_instruments, n_trials))
+file_tensor = np.zeros((n_archs, n_instruments, n_trials))
 
 for arch in range(n_archs):
     for trial in range(n_trials):
         loss_tensor[arch, :, trial] = loss_report[trial][arch][0]
         chunk_tensor[arch, :, trial] = chunk_report[trial][arch]
+        file_tensor[arch, :, trial] = file_report[trial][arch]
 
-# Get the average performance on trials
-average = np.mean(np.mean(chunk_tensor, axis=1), axis=1)
-stddev = np.std(np.mean(chunk_tensor, axis=1), axis=1)
+# Get the chunk-wise average performance on trials
+chunk_avg = np.mean(np.mean(chunk_tensor, axis=1), axis=1)
+chunk_std = np.std(np.mean(chunk_tensor, axis=1), axis=1)
 
-# Get the trials with best training loss
-best_loss_ids = np.argmax(loss_tensor, axis=2)
-best_trials = np.zeros((n_archs, n_instruments))
-for arch in range(n_archs):
-    best_trials[arch] = chunk_tensor[arch, :, best_loss_ids[arch]]
-
-
-# Get the training loss corresponding to best trial
-best_losses = np.zeros((n_archs,))
-best_trial_ids = np.argmax(np.mean(chunk_tensor, axis=1), axis=1)
-for arch in range(n_archs):
-    best_losses[arch] = loss_tensor[arch, 0, best_trial_ids[arch]]
+# Get the file-wise average performance on trials
+file_avg = np.mean(file_tensor, axis=2)
+file_std = np.std(file_tensor, axis=2)
