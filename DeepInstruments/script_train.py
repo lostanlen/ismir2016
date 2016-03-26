eedf1284637e2bff8b9ea1fa532a54894d06d920
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-26 03:25:38 +0100

    finish writingg conclusion

--- DeepInstruments/script_train.py
+++ DeepInstruments/script_train.py
@@ -14,7 +14,7 @@ X_width = decision_length / hop_length
 dense2_channels = 8
 names = [name.split(" ")[0] for name in di.singlelabel.names]
 
-n_trials = 1
+n_trials = 10
 conv1_height = [5, 3] # resp for 2d, spiral
 conv1_width = 3
 pool1_height = 2
@@ -29,8 +29,9 @@ js = np.matrix([[0, 8], [5, 8], [1, 3], [2, 4], [3, 5]])
 loss_report = []
 chunk_report = []
 file_report = []
+report = {}
 
-for trial in range(n_trials):
+for trial in range(4, n_trials):
     print "*********************************************************"
     print "                           TRIAL", 1+trial
     loss_trial = []
@@ -144,4 +145,7 @@ for trial in range(n_trials):
     loss_report.append(loss_trial)
     chunk_report.append(chunk_trial)
     file_report.append(file_trial)
-    pickle.dump(loss_report, open("Table2.p", "wb"))
+    report["loss"] = loss_report
+    report["chunk"] = chunk_report
+    report["file"] = file_report
+    pickle.dump(report, open("Table2.p", "wb"))
